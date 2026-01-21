6756d59ee592cefa952a5bf42508be43c3010ca9
Vishal Kumar Singh <codewithvishaldgp@gmail.com>
2026-01-21 23:54:58 +0530

    Implement cycle detection in linked list

--- L-141.cpp
+++ L-141.cpp
@@ -1,1 +1,21 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slw = head;
+        ListNode* fst = head;
+        while(fst != nullptr && fst->next != nullptr){
+            if(fst->next == slw) return true;
+            slw = slw->next;
+            fst = fst->next->next;
+        }
+        return false;
+    }
+};
