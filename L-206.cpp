416033a6a29ce51bb79a18f50c4a9758a6755935
Vishal Kumar Singh <codewithvishaldgp@gmail.com>
2026-01-21 23:56:41 +0530

    Implement reverse linked list function

--- L-206.cpp
+++ L-206.cpp
@@ -1,1 +1,28 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* ans;
+    ListNode* travel(ListNode* curr, ListNode* prev){
+        if(curr == nullptr){
+            return prev;
+        }
+        ListNode* temp = curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=temp;
+        return travel(curr,prev);
+    }
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        return travel(head, prev);
+    }
+};
